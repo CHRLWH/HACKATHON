3e8d6e1c17dc19c410747862ded4f951fc006f62
CHRLWH <carloshimp@gmail.com>
2025-01-22 23:50:29 +0100

    COMMIT

--- phpessentials/sesioncheck.php
+++ phpessentials/sesioncheck.php
@@ -1,12 +1,11 @@
 <?php
-// Verificar si la sesión está activa
+
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    // Redirigir al login si no está iniciada la sesión
-    header("Location: http://localhost/HACKATHON/Login/LoginHtml/Login.php"); // Cambia "/ruta/a/tu/login.php" por la ruta real
-    exit; // Detener la ejecución del script
+
+    header("Location: http://localhost/HACKATHON/Login/LoginHtml/Login.php");
+    exit;
 }
 
-// Si la sesión está activa, el usuario permanece en la página
 echo "Bienvenido, " . htmlspecialchars($_SESSION['user_name']) . "!";
 ?>
