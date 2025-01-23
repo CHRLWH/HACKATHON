face1d00fdafb71411c6d840d224db8a4be1a034
CHRLWH <carloshimp@gmail.com>
2025-01-23 01:53:24 +0100

    COMMIT

--- ProductoTiendaIvan/PHP/TiendaV2.php
+++ ProductoTiendaIvan/PHP/TiendaV2.php
@@ -74,7 +74,10 @@ function getEstadoClass($tipo) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crafter</title>
+    <title>Hilan-Tienda</title>
+    <link href="../LoginCss/Login.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../../img/1-2feccb09.ico">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/cssT.css">
 </head>
