ba2256abfd793edb99ca1029522a38aa17663ecc
Carlos Hernandez <102351215+CHRLWH@users.noreply.github.com>
2025-01-30 11:27:54 +0100

    commit

--- phpessentials/sesioncheck.php
+++ phpessentials/sesioncheck.php
@@ -5,6 +5,4 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: http://localhost/HACKATHON/Login/Login.php");
     exit;
 }
-
-echo "Bienvenido, " . htmlspecialchars($_SESSION['user_name']) . "!";
 ?>
